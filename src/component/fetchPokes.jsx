485d3dd427f0e6df20d340172a93507b5c50d573
Gray-Desktop <gray.boulware@gmail.com>
2023-04-12 13:26:59 -0500

    updated fetch to axios

--- src/component/fetchPokes.jsx
+++ src/component/fetchPokes.jsx
@@ -1,5 +1,6 @@
 //with endpoint "https://pokeapi.co/api/v2/pokemon"
 import React, { useState, createContext } from 'react';
+import axios from 'axios';
 
 export const PokeContext = createContext();
 
@@ -7,23 +8,36 @@ const FetchPokes = () => {
     //use state to store the Pokemon data
     const [pokemonData, setPokemonData] = useState([]);
 
-    function fetchPokemonData() {
-        fetch("https://pokeapi.co/api/v2/pokemon")
-            .then(response => {
-                const pokemonRawData = response.json();
-                return pokemonRawData;
-            }).then(pokemonRawData => {
-                const pokemonJsonData = pokemonRawData.results;
-                const parsedPokes = pokemonJsonData.map(pokemon => {
-                    let newPoke = [];
-                    newPoke.push(pokemon.name);
-                    console.log(pokemon.name);
-                    return newPoke;
-                })
-                setPokemonData(parsedPokes);                
+    // function fetchPokemonData() {
+    //     fetch("https://pokeapi.co/api/v2/pokemon")
+    //         .then(response => {
+    //             const pokemonRawData = response.json();
+    //             return pokemonRawData;
+    //         }).then(pokemonRawData => {
+    //             const pokemonJsonData = pokemonRawData.results;
+    //             const parsedPokes = pokemonJsonData.map(pokemon => {
+    //                 let newPoke = [];
+    //                 newPoke.push(pokemon.name);
+    //                 console.log(pokemon.name);
+    //                 return newPoke;
+    //             })
+    //             setPokemonData(parsedPokes);                
                 
-            });
+    //         });
 
+    // }
+
+    //update to use axios
+
+    async function fetchPokemonData() {
+        const results = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        const jsonPokemonData = results.data.results;
+        const parsedPokemonNames = jsonPokemonData.map(pokemon => {
+            let newPoke = [];
+            newPoke.push(pokemon.name);
+            return newPoke;
+        })
+        setPokemonData(parsedPokemonNames);
     }
 
     return (
